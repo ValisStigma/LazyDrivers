016e1c18a0280341d9eb1cf1b92cb1c11beee108
Tobi <tschmitz@hsr.ch>
2015-11-05 00:01:53 +0100

    class mag

--- src/main/java/com/zuehlke/carrera/javapilot/akka/actors/ActorHandler.java
+++ src/main/java/com/zuehlke/carrera/javapilot/akka/actors/ActorHandler.java
@@ -4,7 +4,10 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import com.zuehlke.carrera.javapilot.akka.actors.analyseracer.AnalyseRacer;
-import com.zuehlke.carrera.javapilot.akka.actors.fastracer.FastRacer;
+import com.zuehlke.carrera.javapilot.akka.actors.boostracer.BoostRacer;
+import com.zuehlke.carrera.javapilot.akka.actors.staticracer.StaticRacer;
+import com.zuehlke.carrera.javapilot.akka.actors.interpolationracer.DirectionHistory;
+import com.zuehlke.carrera.javapilot.akka.actors.interpolationracer.InterpolationRacer;
 import com.zuehlke.carrera.javapilot.akka.actors.startracer.StartRacer;
 
 import java.util.HashMap;
@@ -12,24 +15,33 @@ import java.util.Map;
 
 public class ActorHandler extends UntypedActor {
 
-    public Map<String, LazyActorRef> actors = new HashMap<>();
+    public Map<Class, LazyActorRef> actors = new HashMap<>();
+
+    public DirectionHistory directionHistory;
 
     public static Props props( ActorRef pilotActor ) {
         return Props.create(
                 ActorHandler.class, () -> new ActorHandler(pilotActor));
     }
 
+    private LazyActorRef create(LazyCreator<? extends  LazyActor> actorCreate){
+        return new LazyActorRef(getContext().system().actorOf(LazyActor.create(actorCreate)));
+    }
+
     public ActorHandler(ActorRef pilot){
-        LazyActor.setPilot(pilot);
+        LazyActor.setData(pilot, this);
+        directionHistory = new DirectionHistory();
 
-        actors.put("StartRacer", new LazyActorRef(getContext().system().actorOf(StartRacer.props(this))));
-        actors.put("FastRacer", new LazyActorRef(getContext().system().actorOf(FastRacer.props(this))));
-        actors.put("AnalyseRacer", new LazyActorRef(getContext().system().actorOf(AnalyseRacer.props(this))));
+        actors.put(StartRacer.class, create(() -> new StartRacer(this, StaticRacer.class)));
+        actors.put(StaticRacer.class, create(() -> new StaticRacer(this)));
+        actors.put(AnalyseRacer.class, create(() -> new AnalyseRacer(this)));
+        actors.put(BoostRacer.class, create(() -> new BoostRacer(this)));
+        actors.put(InterpolationRacer.class, create(() -> new InterpolationRacer(this, directionHistory)));
 
 
-
-        actors.get("StartRacer").startWork();
-        actors.get("AnalyseRacer").startWork();
+        actors.get(InterpolationRacer.class).startWork();
+        actors.get(StartRacer.class).startWork();
+        actors.get(AnalyseRacer.class).startWork();
     }
 
     @Override
